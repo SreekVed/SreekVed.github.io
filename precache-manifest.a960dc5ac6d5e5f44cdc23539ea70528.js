553e5cb1a55333fa792d726f1191a37037f00953
Sreekar Vedula <sreekar.vedula@gmail.com>
2021-08-24 12:13:59 -0400

    Updates

---
+++ precache-manifest.a960dc5ac6d5e5f44cdc23539ea70528.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fa297ac80660d6d306cd78e8ef2113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9112706fc1bcd354b4bd",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "1c45d1ec50bb34d68663",
+    "url": "/static/js/2.39187940.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.39187940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9112706fc1bcd354b4bd",
+    "url": "/static/js/main.93f6afe4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f801b0f7b3e1a8dfde81e414ad616d43",
+    "url": "/static/media/bofa.f801b0f7.png"
+  },
+  {
+    "revision": "b8653e3a7cd1205d74104bb5c8933e25",
+    "url": "/static/media/resume.b8653e3a.pdf"
+  },
+  {
+    "revision": "1ffc61fd60d7dec2810500aaad943321",
+    "url": "/static/media/sreek.1ffc61fd.jpg"
+  }
+]);
